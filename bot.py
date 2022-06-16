6c6dc3cd9efc717294d280533565009f96fdbfea
Bill Martin <wlm2048+github@gmail.com>
2022-06-16 01:19:20 +0000

    bot updates

---
+++ bot.py
@@ -0,0 +1,131 @@
+from datetime import timedelta
+from party_time import Logger, Character, players
+from re import finditer
+import asyncio
+from discord.ext import commands
+import logging
+import party_time
+import re
+import redis
+import time
+
+redis_server = redis.Redis() # Create access to Redis
+bot = commands.Bot(command_prefix="!") # starts the discord client
+logger = logging.getLogger(__name__)
+party_time.logger.setLevel(logging.DEBUG)
+
+AUTH_TOKEN = str(redis_server.get("AUTH_KEY").decode("utf-8"))
+
+async def take_action(mc: str) -> bool:
+    mc = mc.lower()
+    
+    print(f"Actioning: {mc}")
+    
+    rrm = re.match(r'!roleroll\s+(?P<action>\w+)\s+(?P<the_rest>.*)$', mc, re.IGNORECASE)
+    action = rrm['action']
+    the_rest = rrm['the_rest']
+
+    name_line = "|".join([x.lower() for x in party_time.players]) + "|" + "|".join([x.lower().split(" ",1)[0] for x in party_time.players])
+    if "lock" in action:
+        action_line = "|".join([x.lower() for x in party_time.roles])
+    elif "blacklist" in action:
+        action_line = "|".join([x.lower() for x in party_time.jobs.keys()])
+    else:
+        print(f"Don't know how to handle {action}")
+    
+    name_regex = re.compile(r'\b(' + name_line + r')\b')
+    action_regex = re.compile(r'\b(' + action_line + r')\b')
+    
+    name_match = re.search(name_regex, the_rest)
+    the_rest = re.sub(name_match.group(), "", the_rest)
+    action_items = ",".join([m.group() for m in action_regex.finditer(the_rest)])
+    name = name_match.group().split(" ", 1)[0]
+    
+    if "un" in action:
+        time_d = 1
+    else:
+        time_d = timedelta(hours=8)
+        
+    if "lock" in action:
+        key = "role"
+    else:
+        key = "job"
+
+    print(f"ACTION: {action} - key: {key}, name: {name}, items: {action_items}, expire: {time_d}")
+
+    redis_server.setex(
+        f"{key}:{name}",
+        time_d,
+        value=f"{action_items}"
+    )
+    await asyncio.sleep(1)
+    return True
+
+@bot.event 
+async def on_ready():
+    print(f"Successful Launch!!! {bot.user}")
+    
+# @bot.event
+# async def on_message(message): # event that happens per any message.
+#     print(f"{message.channel}: {message.author}:       {message.author.name}: {message.content}")
+    
+@bot.command()
+async def ping(ctx):
+    await ctx.send("pong")
+    
+# @bot.command()
+# async def roleroll(ctx, *args):
+#     await ctx.send('{} arguments: {}'.format(len(args), ', '.join(args)))
+    
+# @bot.command()
+# async def character(ctx, *args):
+#     if len(args) == 0:
+#         await ctx.send("Assign which character to yourself?")
+#         for i, player in enumerate(players, start=1):
+#             await ctx.send(f" {i}: {player}")
+#         await ctx.send("(use !character <number> to assign)")
+#     elif len(args) == 1:
+        
+@bot.event
+async def on_message(message):
+    if '!roleroll' in message.content.lower():
+        new_args = {}
+        mc = message.content
+        logger.info(f"Parsing command: {mc}")
+        level_match = re.match(r'!roleroll\s+level\s+(?P<level>\d+)(?P<level2>\s*\d+)?(?P<dps>\s*dps)?', mc, re.IGNORECASE)
+        if level_match:
+            logger.info(f"Using level {level_match.group('level')}")
+            new_args['level'] = level_match.group('level')
+            
+        if level_match and level_match.group('level2'):
+            new_args['level2'] = level_match.group('level2')
+            
+        if level_match and level_match.group('dps'):
+            new_args['dps'] = True
+
+        if re.match(r'!roleroll\s+(?:(?:un)?lock|(?:un)?blacklist)\s+', mc, re.IGNORECASE):
+            return await take_action(mc)
+        
+        role_locks = redis_server.keys("role:*")
+        if role_locks:
+            locks = "Current character locks are:\n"
+            for rl in role_locks:
+                pc = (rl.decode('utf-8').split(":", 1))[1]
+                role = redis_server.get(rl).decode('utf-8').split(",")
+                locks = locks + f" * {pc}: {', '.join(role)}\n"
+            await message.channel.send(locks)
+
+        job_locks = redis_server.keys("job:*")
+        if job_locks:
+            locks = "Current character blacklisted jobs are:\n"
+            for rl in job_locks:
+                pc = (rl.decode('utf-8').split(":", 1))[1]
+                jobs = redis_server.get(rl).decode('utf-8').split(",")
+                locks = locks + f" * {pc}: {', '.join(jobs)}\n"
+            await message.channel.send(locks)
+
+        winners = "\n".join(party_time.find_winners(new_args))
+        await message.channel.send(winners)
+        return True
+        
+bot.run(AUTH_TOKEN) # Pull Auth Token from above
--- party_time.py
+++ party_time.py
@@ -9,9 +9,13 @@ from diskcache import Cache
 from pprint import pprint
 import argparse
 import re
+import redis
+
+redis_server = redis.Redis() # Create access to Redis
 
 parser = argparse.ArgumentParser(description='Do the duty!')
 parser.add_argument('-l', '--level', type=int, help='Minimum level to consider', default=16)
+parser.add_argument('-m', '--level2', type=int, help='Maximum level to consider', default=89)
 parser.add_argument('-d', '--dps', action='store_true', help='Prioritize ranged AND melee dps', default=False)
 parser.add_argument('-v', '--verbose', action='count', help='Increase logging verbosity', default=0)
 args = parser.parse_args()
@@ -28,11 +32,57 @@ players = [
     "Pokina Da'eye",
     'Julian Dereschabbot'
 ]
-duty_roles = ['Tank', 'Healer']
-if args.dps:
-    duty_roles.extend(['Melee DPS', 'Physical Ranged DPS'])
-else:
-    duty_roles.extend(['DPS1', 'DPS2'])
+
+roles = [
+    "dps",
+    "healer",
+    "tank"
+]
+
+jobs = {
+    "acn": "arcanist",
+    "arc": "archer",
+    "ast": "astrologian",
+    "blm": "black mage",
+    "blu": "blue mage",
+    "brd": "bard",
+    "cnj": "conjurer",
+    "dnc": "dancer",
+    "drg": "dragoon",
+    "drk": "dark knight",
+    "gla": "gladiator",
+    "gnb": "gunbreaker",
+    "lnc": "lancer",
+    "mch": "machinist",
+    "mnk": "monk",
+    "mrd": "marauder",
+    "nin": "ninja",
+    "pgl": "pugilist",
+    "pld": "paladin",
+    "rdm": "red mage",
+    "rog": "rogue",
+    "rpr": "reaper",
+    "sam": "samurai",
+    "sch": "scholar",
+    "sge": "sage",
+    "smn": "summoner",
+    "thm": "thaumaturge",
+    "war": "warrior",
+    "whm": "white mage"
+}
+
+base_classes = {
+    "Bard": "Archer",
+    "Black Mage": "Thaumaturge",
+    "Dragoon": "Lancer",
+    "Monk": "Pugilist",
+    "Ninja": "Rogue",
+    "Paladin": "Gladiator",
+    "Scholar": "Arcanist",
+    "Summoner": "Arcanist",
+    "Warrior": "Marauder",
+    "White Mage": "Conjurer"
+}
 
 class Logger:
     def getLogger(name: str) -> object:
@@ -52,7 +102,7 @@ class Logger:
             "handlers": {
                 "hand_so": {
                     "class": "logging.StreamHandler",
-                    "level": "NOTSET",
+                    "level": "DEBUG",
                     "formatter": "default",
                     "filters": ["no_errors"],
                     "stream": "ext://sys.stdout"
@@ -100,14 +150,43 @@ class Character:
         self.name = name
         self.get()
 
+    def characters(self: object) -> list:
+        return players
+
     def get(self: object) -> object:
+        logger.info(f"Looking up {self.name}")
         self.id = self.lookup_by_name()
-        self.jobs = self.lookup_by_id()
+        self.role_lock = self.role_locks()
+        self.job_lock = self.job_locks()
+        self.jobs = self.get_jobs()
+
+    def job_locks(self: object) -> object:
+        job_locks = redis_server.keys("job:*")
+        if job_locks:
+            for rl in job_locks:
+                pc = (rl.decode('utf-8').split(":", 1))[1]
+                job = redis_server.get(rl).decode('utf-8').split(",")
+                if pc in self.name.lower():
+                    logger.info(f"Blacklisting {pc} from {job}")
+                    return job
+
+
+    def role_locks(self: object) -> object:
+        role_locks = redis_server.keys("role:*")
+        if role_locks:
+            for rl in role_locks:
+                pc = (rl.decode('utf-8').split(":", 1))[1]
+                role = redis_server.get(rl).decode('utf-8').split(",")
+                if pc in self.name.lower():
+                    logger.info(f"Locking {pc} to {role}")
+                    return role
 
     def not_do(self: object, role: str) -> bool:
         return not self.can_do(role)
 
     def can_do(self: object, role: str) -> bool:
+        if self.role_lock and role.lower() not in self.role_lock:
+            return False
         return role in self.jobs.keys()
 
     def _lookup_by_name(self: object, name: str) -> object:
@@ -141,7 +220,7 @@ class Character:
         entry = soup.find('a', attrs={'class': 'entry__link'})
         return entry['href']
 
-    def lookup_by_id(self: object) -> dict:
+    def get_jobs(self: object) -> dict:
         r = self._lookup_by_id()
         soup = BeautifulSoup(r.content, 'html5lib')
         roles = {}
@@ -158,7 +237,7 @@ class Character:
             for cl in classes:
                 job_name = cl.find('div', attrs={'class': 'character__job__name'}).text
                 job_level = cl.find('div', attrs={'class': 'character__job__level'}).text
-                if job_level != '-' and int(job_level) >= args.level:
+                if job_level != '-' and int(job_level) >= args.level and int(job_level) <= args.level2:
                     c[job_name] = {'level': job_level}
             if c:
                 if role_name in roles:
@@ -168,9 +247,40 @@ class Character:
                     roles[role_name] = c
                 logger.debug(f" - can do {role_name}: {roles[role_name]}")
 
+        # remove base jobs if never progressed
+        for role in roles:
+            for job in roles[role]:
+                if job in base_classes:
+                    for role2 in roles:
+                        if base_classes[job] in roles[role2]:
+                            logger.debug(f"    ** found {job} removing {base_classes[job]}")
+                            del roles[role2][base_classes[job]]
+
+        # remove jobs if blacklisted
+        for role, jobs in roles.items():
+            for job in jobs.copy():
+                if self.job_lock and job.lower() in self.job_lock:
+                    logger.info(f"Removing {job} because of blacklist")
+                    del roles[role][job]
+
         return roles
 
-def main() -> None:
+def find_winners(more_args: dict = {}) -> list:
+    winners = []
+
+    if 'level' in more_args:
+        args.level = int(more_args['level'])
+    if 'level2' in more_args:
+        args.level2 = int(more_args['level2'])
+    if 'dps' in more_args:
+        args.dps = True
+        
+    duty_roles = ['Tank', 'Healer']
+    if args.dps:
+        duty_roles.extend(['Melee DPS', 'Physical Ranged DPS'])
+    else:
+        duty_roles.extend(['DPS1', 'DPS2'])
+
     player_data = {}
     for player in players:
         logger.info(f"Getting job data for player: {player}")
@@ -189,7 +299,10 @@ def main() -> None:
     picked = []
     final = {}
     success = False
+    max_attempts = 100
+    attempts = 0
     while not success:
+        logger.debug("Trying to find a group... ")
         for role, who in sorted_whocan.items():
             while True:
                 random.shuffle(who)
@@ -203,6 +316,14 @@ def main() -> None:
             final[role] = lucky
         if len(set(final.values())) == 4:
             success = True
+        if attempts >= max_attempts:
+            break
+        attempts = attempts + 1
+
+    if success is False:
+        return([f"Could not find a solution after {attempts} attempts"])
+    else:
+        logger.info(f"Found a solution after {attempts} attempts")
 
     for role in duty_roles:
         _role = role
@@ -211,7 +332,16 @@ def main() -> None:
         jobs = dict(player_data[final[role]].jobs)[_role]
         j = list(jobs.keys())
         random.shuffle(j)
-        print(f"{final[role]}: {role} ({j[0]} - {jobs[j[0]]['level']})")
+        winner = f"{final[role]}: {role} ({j[0]} - {jobs[j[0]]['level']})"
+        logger.info(winner)
+        winners.append(winner)
+
+    return winners
+
+def main() -> None:
+    winners = find_winners()
+    for w in winners:
+        print(w)
 
 if __name__ == '__main__':
     main()
